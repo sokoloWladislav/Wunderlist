7ab9f12740e7315e9e2519fb036cbe54b539ff67
IvanPuchkov <ivan.puchkov1995@gmail.com>
2016-03-16 01:33:02 +0300

    remove additionl conflict

--- Wunderlist/UI/App_Start/BundleConfig.cs
+++ Wunderlist/UI/App_Start/BundleConfig.cs
@@ -5,7 +5,6 @@ namespace UI
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -13,9 +12,7 @@ namespace UI
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/normalize.css",
                       "~/Content/site.css"));
